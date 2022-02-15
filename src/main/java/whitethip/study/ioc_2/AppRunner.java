550ad12d9170c1a0ee2e469631dff988c81ed85e
ddgg9511@naver.com <ddgg9511@naver.com>
2022-02-15 22:09:43 +0900

    spEl(Spring Expression Language)

--- src/main/java/whitethip/study/ioc_2/AppRunner.java
+++ src/main/java/whitethip/study/ioc_2/AppRunner.java
@@ -1,9 +1,13 @@
 package whitethip.study.ioc_2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
@@ -13,11 +17,40 @@ import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("${my.value}")
+    int myValue;
+
+    @Value("#{${my.value} eq 1}")
+    boolean isMyValue1;
+
+    @Value("#{'spring'}")
+    String spring;
+
+    @Value("#{sample.data}")
+    int sampleDate;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService.getClass().toString());
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(myValue);
+        System.out.println(isMyValue1);
+        System.out.println(spring);
+        System.out.println(sampleDate);
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("2 + 100");
+        Integer value = expression.getValue(Integer.class);
+        System.out.println(value);
     }
 }
